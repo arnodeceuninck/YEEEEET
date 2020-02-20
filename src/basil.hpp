792435a8292da9d0707184dfbf505e72af6647e7
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-02-20 19:42:18 +0100

    made functions

--- src/basil.hpp
+++ src/basil.hpp
@@ -12,9 +12,25 @@ void output(std::string filename, std::vector<Library*>& libraries)
     std::ofstream outputFile(filename);
 
     outputNumberOfLibraries(filename, libraries);
+
+    for (auto library: libraries) {
+        // Mateo
+        outputAmountOfBooks(filename, library);
+
+        // Basil
+        outputBookNumbers(filename, library);
+    }
 }
 
 void outputNumberOfLibraries(std::ofstream& filename, std::vector<Library*>& libraries)
 {
-    outputFile << libraries.size();
+    outputFile << libraries.size() << std::endl;
+}
+
+void outputAmountOfBooks(std::ostream& outputFile, Library* library) {
+
+}
+
+void outputBookNumbers(std::ostream& outputFile, Library* library) {
+
 }
