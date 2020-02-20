a1ac2fc0f769db46fa857a39ec5f965d768550be
sharogue <mano.marichal@student.uantwerpen.be>
2020-02-20 18:48:56 +0100

    belangrijk

--- mano.cpp
+++
@@ -1,9 +0,0 @@
-// =====================================================================
-// @name: mano.cpp
-// @project: YEEEEET
-// @author: Mano Marichal
-// @date: 20.02.20
-// @copyright: BA2 Informatica - Mano Marichal - University of Antwerp
-// @description: hashcode 2020
-// =====================================================================
-
