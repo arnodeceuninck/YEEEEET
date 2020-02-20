3eb8f024f878c1312b400b39d1ba4e4c2d1d0774
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-02-20 19:31:34 +0100

    added basil.hpp

---
+++ src/basil.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file basil.cpp
+ * @author Basil Rommens
+ * @date 2/20/20
+ * @brief 
+ */
+
+#include "mano.hpp"
+
+void output(std::string filename, std::vector<Library*> libraries) {
+    std::ifstream outputFile(filename);
+
+    // number of libraries
+    outputFile << libraries.size();
+    outputFile <<
+}
